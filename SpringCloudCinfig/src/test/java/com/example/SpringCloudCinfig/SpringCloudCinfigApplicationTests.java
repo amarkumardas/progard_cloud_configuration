7c5d6cc6970389dc4aba2ff0fb8414f2265d6c15
Amar Kumar Das <85432034+amarkumardas@users.noreply.github.com>
2022-08-10 22:19:46 +0530

    completed

---
+++ SpringCloudCinfig/src/test/java/com/example/SpringCloudCinfig/SpringCloudCinfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringCloudCinfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudCinfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
